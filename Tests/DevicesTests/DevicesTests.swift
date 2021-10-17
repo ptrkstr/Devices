af25f4ca567a8c52fee08b5e261a98b001361a58
ptrkstr <11362913+ptrkstr@users.noreply.github.com>
2021-10-17 16:30:10 +0400

    WIP

--- Tests/DevicesTests/DevicesTests.swift
+++ Tests/DevicesTests/DevicesTests.swift
@@ -1,53 +1,16 @@
-//import XCTest
-//@testable import Devices
-//import SwiftSoup
-//import Slab
-//
-//// TODO: Download HTML doc, don't store it
-//
-//final class DevicesTests: XCTestCase {
-//    
-//    func testAAA() {
-////        aaa()
-//        
-//    }
-//    
-//    func testExample() throws {
-//        let url = Bundle.module.resourceURL!.appendingPathComponent("Models - The iPhone Wiki.html")
-//        let html = try String(contentsOf: url, encoding: .utf8)
-//        let doc = try SwiftSoup.parse(html)
-//        let tables = try doc.getElementsByTag("table")
-//        let slab = Slab()
-//        
-//        let aaa = try slab.convert(tables[1].outerHtml())
-//        let serialise = try JSONSerialization.data(withJSONObject: aaa, options: [])
-//        let airpods = try JSONDecoder().decode([Airpod].self, from: serialise)
-//        
-//        print("[")
-//        for airpod in airpods {
-//            print("\t\(try encode(airpod)),")
-//        }
-//        print("].lazy")
-//        
-////        print()
-//    }
-//    
-//    
-//    
-//    func encode(_ airpod: Airpod) throws -> String {
-//        let airpod = Mirror(reflecting: airpod)
-//        let params = try airpod.children.map { child in
-//            let value: String = try {
-//                switch child.value {
-//                case let array as [String]: return "[\(array.map { "\"\($0)\"" }.joined(separator: ", "))]"
-//                case let string as String: return "\"\(string)\""
-//                default: throw NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Unknown type found during encoding"])
-//                }
-//            }()
-//            return "\(child.label!): \(value)"
-//        }
-//            .joined(separator: ", ")
-//        
-//        return ".init(\(params))"
-//    }
-//}
+import XCTest
+@testable import Devices
+import SwiftSoup
+import Slab
+
+// TODO: Download HTML doc, don't store it
+
+final class DevicesTests: XCTestCase {
+    
+    func testAAA() {
+        iPhone.all.forEach {
+            print($0.identifier)
+        }
+    }
+
+}
